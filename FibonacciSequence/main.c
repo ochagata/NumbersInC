15914073cb5f00d6ca3ab13b3c6448517f644bcd
unknown <chagatai@1USL22893.NOE.Nokia.com>
2014-07-07 18:09:34 +0100

    Fibonacci Sequence done

---
+++ FibonacciSequence/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main()
+{
+	int input;
+	printf("Enter a number and have the program generate the "
+			"Fibonacci sequence to that number or to the Nth number: ");
+	scanf("%d", &input);
+	if(input < 0 || input > 47)
+	{
+		printf("Error! Error!"); // Send error if the input is faulty
+	}
+	else
+	{
+		unsigned long output = 0;
+		unsigned long lastNumber = 0;
+		int i;
+		for( i = 0; i < input; i++)
+		{
+			if(input == 1 || i == 1)
+			{
+				output = 1;
+			}
+			output += lastNumber;
+			lastNumber = output - lastNumber;
+		}
+		printf("%lu", output);
+	}
+	return 0;
+}
+
+
+//sernatin
+//skunkworks1
